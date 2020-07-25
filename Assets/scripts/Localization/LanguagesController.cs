c8f1cbd1cb2501a8b63cfaac046a6cf680882c28
strategimws <elgin9494@mail.ru>
2020-07-25 12:30:00 +0700

    Project V1.0
    Заливка проекта на GitHub

---
+++ Assets/scripts/Localization/LanguagesController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanguagesController : MonoBehaviour
+{
+    /*
+        1 - Русский
+        2 - Английский
+        3 - Испанский
+        4 - Португальский
+        5 - Немецкий
+        6 - Французский
+        7 - Польский
+        8 - Финский
+        9 - Шведский
+        10 - Норвежский
+        11 - Латвийский
+        12 - Литовский
+        13 - Эстонский
+        14 - Корейский
+        15 - Японский
+        16 - Индийский
+        17 - Китайский
+      
+    */
+
+    private int language;
+
+    private void languageSettings()
+    {
+        if (Application.systemLanguage == SystemLanguage.Russian)
+            language = 1;
+        else if (Application.systemLanguage == SystemLanguage.English)
+            language = 2;
+        else if (Application.systemLanguage == SystemLanguage.Spanish)
+            language = 3;
+        else if (Application.systemLanguage == SystemLanguage.Portuguese)
+            language = 4;
+        else if (Application.systemLanguage == SystemLanguage.German)
+            language = 5;
+        else if (Application.systemLanguage == SystemLanguage.French)
+            language = 6;
+        else if (Application.systemLanguage == SystemLanguage.Polish)
+            language = 7;
+        else if (Application.systemLanguage == SystemLanguage.Finnish)
+            language = 8;
+        else if (Application.systemLanguage == SystemLanguage.Swedish)
+            language = 9;
+        else if (Application.systemLanguage == SystemLanguage.Norwegian)
+            language = 10;
+        else if (Application.systemLanguage == SystemLanguage.Latvian)
+            language = 11;
+        else if (Application.systemLanguage == SystemLanguage.Lithuanian)
+            language = 12;
+        else if (Application.systemLanguage == SystemLanguage.Estonian)
+            language = 13;
+        else if (Application.systemLanguage == SystemLanguage.Korean)
+            language = 14;
+        else if (Application.systemLanguage == SystemLanguage.Japanese)
+            language = 15;
+        else if (Application.systemLanguage == SystemLanguage.Italian)
+            language = 16;
+        else
+            language = 17;
+    }
+    
+    private void Start()
+    {
+        languageSettings();
+        StaticSettings.languages = language;
+    }
+
+}
---
+++ Assets/scripts/Localization/LocalizationController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizationController : MonoBehaviour
+{
+    [SerializeField]
+    private string[] translateText;
+    [SerializeField]
+    private Text textLanguage;
+
+    private int localization;
+
+
+    private void translating()
+    {
+        localization = StaticSettings.languages;
+        switch (localization)
+        {
+            case 1: textLanguage.text = translateText[localization - 1]; break;
+            case 2: textLanguage.text = translateText[localization - 1]; break;
+            case 3: textLanguage.text = translateText[localization - 1]; break;
+            case 4: textLanguage.text = translateText[localization - 1]; break;
+            case 5: textLanguage.text = translateText[localization - 1]; break;
+            case 6: textLanguage.text = translateText[localization - 1]; break;
+            case 7: textLanguage.text = translateText[localization - 1]; break;
+            case 8: textLanguage.text = translateText[localization - 1]; break;
+            case 9: textLanguage.text = translateText[localization - 1]; break;
+            case 10: textLanguage.text = translateText[localization - 1]; break;
+            case 11: textLanguage.text = translateText[localization - 1]; break;
+            case 12: textLanguage.text = translateText[localization - 1]; break;
+            case 13: textLanguage.text = translateText[localization - 1]; break;
+            case 14: textLanguage.text = translateText[localization - 1]; break;
+            case 15: textLanguage.text = translateText[localization - 1]; break;
+            case 16: textLanguage.text = translateText[localization - 1]; break;
+            default: textLanguage.text = translateText[1]; break;
+        }
+    }
+
+    private void Start()
+    {
+        translating();
+    }
+}
